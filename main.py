6c693ba0eeb50e9b9d4f705a12edd03dba8b96f0
z-Hadi <zaid.zan1124@gmail.com>
2023-06-12 17:55:26 +0100

    adding the headings

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import requests
+import re
 from bs4 import BeautifulSoup
-url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000"
+url = "http://books.toscrape.com/catalogue/the-last-mile-amos-decker-2_754/index.html"
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.prettify())
 
-print(soup)
+universal_product_code= soup.find("th", string="UPC" ).next_sibling.get_text()
+book_title= soup.find('li', class_="active").get_text()
+price_including_tax= soup.find("th", string="Price (excl. tax)" ).next_sibling.get_text()
+price_excluding_tax= soup.find("th", string="Price (incl. tax)" ).next_sibling.get_text()
+quantity_available= soup.find(string=re.compile("In stock"))[24:27]
+image_url =soup.find('img', attrs={"alt": book_title})['src']
+
+
+
+test= soup.find("div", attrs={"id": "product_description"}).next_sibling
+
 # #
 # #Get string of HTML title
 # print(soup.title.string)
