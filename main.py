ed10966f771a32c5aea1d782d4b03b60b414b06c
z-hadi <zaid.zan1124@gmail.com>
2023-06-17 18:39:53 +0100

    Scrapping all headers

--- main.py
+++ main.py
@@ -1,41 +1,34 @@
 import requests
 import re
-from bs4 import BeautifulSoup
-url = "http://books.toscrape.com/catalogue/the-last-mile-amos-decker-2_754/index.html"
+import csv
+from bs4 import BeautifulSoup, Comment
+
+headers = ["product_page_url", "universal_product_code", "book_title", "price_incl_tax", "price_excl_tax" , "qnt_available", "product_description", "category",  "review_rating", "image_url"]
+url = "https://books.toscrape.com/catalogue/the-last-mile-amos-decker-2_754"
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
-
+# print(soup.prettify())
+product_page_url =(soup.find('p', class_="instock availability").find_next_sibling("p").contents[11].replace('<small><a href="','').replace('/reviews/">','').replace('0 customer reviews','').replace('</a></small>','').replace('\n','').replace(' /catalogue/','https://books.toscrape.com/catalogue/'))
 universal_product_code= soup.find("th", string="UPC" ).next_sibling.get_text()
 book_title= soup.find('li', class_="active").get_text()
-price_including_tax= soup.find("th", string="Price (excl. tax)" ).next_sibling.get_text()
-price_excluding_tax= soup.find("th", string="Price (incl. tax)" ).next_sibling.get_text()
-quantity_available= soup.find(string=re.compile("In stock"))[24:27]
-image_url =soup.find('img', attrs={"alt": book_title})['src']
+price_incl_tax= soup.find("th", string="Price (excl. tax)" ).next_sibling.get_text()
+price_excl_tax= soup.find("th", string="Price (incl. tax)" ).next_sibling.get_text()
+qnt_available= soup.find(string=re.compile("In stock"))[24:26]
+product_description = soup.find("div", attrs={"id": "product_description", "class": "sub-header"}).find_next_sibling("p").get_text()
+category = soup.find("ul", class_="breadcrumb").find_all("a", limit=3)[-1].get_text()
+review_rating= str(soup.find('p', class_="instock availability").find_next_sibling("p")["class"][-1])+" stars"
+image_url =(soup.find('img', attrs={"alt": book_title})['src'].replace('../../media/','https://books.toscrape.com/catalogue/media/'))
+
+headers_values = ["product_page_url", "universal_product_code", "book_title", "price_incl_tax", "price_excl_tax" , "qnt_available", "product_description", "category",  "review_rating", "image_url"]
+
+with open('data.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(headers)
+    for i in range(len(headers)):
+        writer.writerow(str(headers_values[i]))
 
 
 
-test= soup.find("div", attrs={"id": "product_description"}).next_sibling
-
-# #
-# #Get string of HTML title
-# print(soup.title.string)
-# #Find all elements with <a> tag
-# print(soup.find_all('a'))
-# # Find element with id of “link1”
-# print(soup.find(id="link1"))
-# #Find all p elements with class “title”
-# print(soup.find_all("p", class_="title"))
-#
-# titles = soup.find_all("a" , class_="gem-c-document-list__item-title")
-#
-# for title in titles:
-#  print(title.string)
-
-# bs_titles = soup.find_all("a", class_="gem-c-document-list__item-title")
-# titles = []
-# for title in bs_titles:
-#     titles.append(title.string)
 
 
