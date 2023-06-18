6e2b850faf81961d9b2ed5790b7cfd39b65b04a6
z-Hadi <zaid.zan1124@gmail.com>
2023-06-18 20:42:07 +0100

    Phase 1

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import csv
 from bs4 import BeautifulSoup, Comment
 
 headers = ["product_page_url", "universal_product_code", "book_title", "price_incl_tax", "price_excl_tax" , "qnt_available", "product_description", "category",  "review_rating", "image_url"]
-url = "https://books.toscrape.com/catalogue/the-last-mile-amos-decker-2_754"
+url = "http://books.toscrape.com/catalogue/murder-at-the-42nd-street-library-raymond-ambler-1_624"
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 # print(soup.prettify())
-product_page_url =(soup.find('p', class_="instock availability").find_next_sibling("p").contents[11].replace('<small><a href="','').replace('/reviews/">','').replace('0 customer reviews','').replace('</a></small>','').replace('\n','').replace(' /catalogue/','https://books.toscrape.com/catalogue/'))
+product_page_url = soup.find_all(string=lambda string:isinstance(string, Comment))[9].replace('\n','').replace(" ","").replace('<aid="write_review"href="/','').replace('/reviews/add/#addreview"class="btnbtn-successbtn-sm">Writeareview</a>','').replace('catalogue/','https://books.toscrape.com/catalogue/')
 universal_product_code= soup.find("th", string="UPC" ).next_sibling.get_text()
 book_title= soup.find('li', class_="active").get_text()
 price_incl_tax= soup.find("th", string="Price (excl. tax)" ).next_sibling.get_text()
@@ -18,17 +18,11 @@ qnt_available= soup.find(string=re.compile("In stock"))[24:26]
 product_description = soup.find("div", attrs={"id": "product_description", "class": "sub-header"}).find_next_sibling("p").get_text()
 category = soup.find("ul", class_="breadcrumb").find_all("a", limit=3)[-1].get_text()
 review_rating= str(soup.find('p', class_="instock availability").find_next_sibling("p")["class"][-1])+" stars"
-image_url =(soup.find('img', attrs={"alt": book_title})['src'].replace('../../media/','https://books.toscrape.com/catalogue/media/'))
+image_url =(soup.find('img', attrs={"alt": book_title})['src'].replace('../../media/','https://books.toscrape.com/media/'))
 
-headers_values = ["product_page_url", "universal_product_code", "book_title", "price_incl_tax", "price_excl_tax" , "qnt_available", "product_description", "category",  "review_rating", "image_url"]
+headers_values = [product_page_url, universal_product_code, book_title, price_incl_tax, price_excl_tax , qnt_available, product_description, category,  review_rating, image_url]
 
 with open('data.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     writer.writerow(headers)
-    for i in range(len(headers)):
-        writer.writerow(str(headers_values[i]))
-
-
-
-
-
+    writer.writerow(headers_values)
